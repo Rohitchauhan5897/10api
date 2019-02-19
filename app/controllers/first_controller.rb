8619f138b3b5e1c05626c7ce40b5058e6c0aa34e
Rohit Kumar <rohit.2019@tecorb.co>
2019-02-19 16:22:46 +0530

    hello

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -3,18 +3,18 @@ before_action  :current_user , except: [:create_user,:login]
 
 	def create_user
 		@user1 =get_user(params[:email])
-        @user2 =get_user2(params[:username])
+        	@user2 =get_user2(params[:username])
        	if !@user1.present? && !@user2.present?
-        	  user =User1.create!(firstname:params[:firstname], lastname:params[:lastname], username:params[:username],email:params[:email], contact_no: params[:contact_no], gender:params[:gender], dob:params[:dob])
-           		if user.present?
+        		  user =User1.create!(firstname:params[:firstname], lastname:params[:lastname], username:params[:username],email:params[:email], contact_no: params[:contact_no], gender:params[:gender], dob:params[:dob])
+           		  if user.present?
            			Auth1.create!(password:params[:password],user1_id: user.id)	
               		render json:{code:200, message: "User Signed Up Successfully!", user:user}
-        		else
+        		   else
         			render json:{code:500, message: "Bad Request!!"}
-        		end
-        else
-         	render json:{code:400, message: "User already exists in the database!"}
-    	end
+  			   end
+            else
+         		   render json:{code:400, message: "User already exists in the database!"}
+	    	end
 	end
 
 	def login
@@ -24,9 +24,9 @@ before_action  :current_user , except: [:create_user,:login]
 		if userinfo.present? && auth.authenticate(params[:password])
 			@temp=SecureRandom.hex
 			if !@token.present?
-			 session[:auth1_id]=@temp
-			 @session=Session1.create!(token:@temp,auth1_id:auth.id)
-			render json:{code:200,message:"logged in successfully", userinfo:userinfo.first}
+			 	session[:auth1_id]=@temp
+				@session=Session1.create!(token:@temp,auth1_id:auth.id)
+				render json:{code:200,message:"logged in successfully", userinfo:userinfo.first}
 			end
 		else
 		    render json:{code:400,message:"Enter Correct Username or Password"}	
@@ -36,23 +36,24 @@ before_action  :current_user , except: [:create_user,:login]
 	 def destroy
 	 	token = Session1.find_by_token(params[:token])
 	 	if token.present?
-   	   	token.destroy
-	 	   render json:{code:200, message: "The user has been succesfully Logout..!"}
-	 	 else
+   	   		token.destroy
+	 	   	render json:{code:200, message: "The user has been succesfully Logout..!"}
+	 	else
 	 	 	render json:{code:400, message: "Id not found"}
-  end	
-end
-
+  		end	
+	end
 
 	def updateprofile
-		changeinfo=User1.find_by_id(params[:id])
-       if changeinfo.update(firstname:params[:firstname], lastname:params[:lastname], username:params[:username], email:params[:email], contact_no:params[:contact_no],gender:params[:gender],dob:params[:dob])
-       		render json:{code:200,message:"Profile Update successfully", changeinfo:changeinfo}
-       else
+		token=Session1.find_by_token(params[:token])
+		user = token.present? ? token.auth1.user1 : nil
+		p token.auth1.user1
+		if user.present?
+			user.update(firstname:params[:firstname], lastname:params[:lastname], contact_no:params[:contact_no],gender:params[:gender],dob:params[:dob])
+			render json:{code:200,message:"Profile Update successfully", user:user}
+      	 else
        		render json:{code:400,message:"Profile not Update"}	
 		end 
 	end
-
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	post "first/create_user" => "first#create_user" 
 	post "first/login" => "first#login"
 	get  "first/destroy/token/:token" => "first#destroy"
-	post "first/updateprofile/:id" => "first#updateprofile"
+	post "first/updateprofile" => "first#updateprofile"
+
 end
