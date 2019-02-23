fe88afaf73336a3288d84bfe20c81435f3fa83a7
Rohit Kumar <rohit.2019@tecorb.co>
2019-02-23 11:12:05 +0530

    hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
    authentication:       'plain',
    enable_starttls_auto: true  
 }
-config.action_mailer.default_url_option = { host: "http://localhost:3000/"}
+#config.action_mailer.default_url_option = { host: "http://localhost:3000/"}
 end
